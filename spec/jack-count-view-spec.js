8588a1f7567af2e647fdcd453dbcc48097e74163
unix1024 <unix1024@163.com>
2023-08-05 17:19:20 +0800

    2 commit

--- spec/jack-count-view-spec.js
+++ spec/jack-count-view-spec.js
@@ -4,6 +4,6 @@ import JackCountView from '../lib/jack-count-view';
 
 describe('JackCountView', () => {
   it('has one valid test', () => {
-    expect('life').toBe('easy');
+    expect('life').toBe('life');
   });
 });
